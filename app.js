958a6f08cea59f015897d1342640e52c620a74c4
linlinlearning <linlinlearning@gmail.com>
2023-07-24 10:32:56 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -1,4 +1,16 @@
 const express = require('express')
+const mongoose = require('mongoose')
+
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config()
+  }
+
+mongoose.connect(process.env.MONGODB_URI)
+
+const db = mongoose.connection
+db.on('error', () => { console.log('mongodb error!') })
+db.once('open', () => { console.log('mongodb connected!') })
+  
 const app = express()
 
 const port = 3000
