4958eb26cacda90d169a8ef66da148c58f865a7e
linlinlearning <linlinlearning@gmail.com>
2023-07-24 16:04:42 +0800

    feat add express handlebars

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const express = require('express')
 const mongoose = require('mongoose')
-const shorten = require('./models/shorten.js')
+const exphbs = require('express-handlebars')
 
+// include Record model
+const Record = require('./models/record.js')
+
+// include shorten function
+const shorten = require('./models/shorten.js')
 
 if (process.env.NODE_ENV !== 'production') {
     require('dotenv').config() 
@@ -14,11 +19,13 @@ db.on('error', () => { console.log('mongodb error!') })
 db.once('open', () => { console.log('mongodb connected!') })
   
 const app = express()
-
 const port = 3000
 
+app.engine('hbs', exphbs.engine({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
+
 app.get('/', (req, res) => {
-    res.send('Hello user!')
+    res.render('index')
 })
 
 app.listen(port, () => {
