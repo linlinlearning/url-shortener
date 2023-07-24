0e5dafac9d1517259095e7658c9b7207d40434b2
linlinlearning <linlinlearning@gmail.com>
2023-07-24 16:11:52 +0800

    feat: add static files in folder public

--- app.js
+++ app.js
@@ -23,6 +23,7 @@ const port = 3000
 
 app.engine('hbs', exphbs.engine({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
+app.use(express.static('public'))
 
 app.get('/', (req, res) => {
     res.render('index')
