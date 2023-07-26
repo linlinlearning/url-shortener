14f67c3c62ec84b946348cb2ef8172a2c30e2723
linlinlearning <linlinlearning@gmail.com>
2023-07-26 17:52:23 +0800

    doc: add comments to facilitate reading

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// include packages
 const express = require('express')
 const mongoose = require('mongoose')
 const exphbs = require('express-handlebars')
@@ -9,23 +10,28 @@ const Record = require('./models/record.js')
 // include shorten function
 const shorten = require('./utils/shorten.js')
 
+// database-related settings 
 if (process.env.NODE_ENV !== 'production') {
     require('dotenv').config() 
   }
-
 mongoose.connect(process.env.MONGODB_URI)
 const db = mongoose.connection
 db.on('error', () => { console.log('mongodb error!') })
 db.once('open', () => { console.log('mongodb connected!') })
-  
+
+// create the app server
 const app = express()
 const port = 3000
 
+// use view engine 'express-handlebars' ('hbs')
 app.engine('hbs', exphbs.engine({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
-app.use(express.static('public'))
-app.use(bodyParser.urlencoded({ extended: true }))
 
+// use the static files in folder 'public'
+app.use(express.static('public'))
+
+// use body-parser to process form contents
+app.use(bodyParser.urlencoded({ extended: true }))
 
 // define route for index page
 app.get('/', (req, res) => {
@@ -35,8 +41,11 @@ app.get('/', (req, res) => {
 // define route for adding a URL
 app.post('/records', async (req, res) => {
     const url_full = req.body.url_full
+    
+    // look up the input URL in the Record model 
     const result = await Record.find({ url_full: url_full}).exec()
 
+    // if the input URL exists, use its short URL; else, use the shorten function to generate a short URL
     if (result.length > 0) {
         const foundData = result[0]
         res.render('success', { url_full: url_full, url_short: foundData.url_short})
